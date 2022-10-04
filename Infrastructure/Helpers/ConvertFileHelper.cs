2e918755bb37211d6d433cda11e150d09fb9c7a1
Эмиль Воробьёв <vorobevemil@gmail.com>
2022-10-04 12:50:39 +0300

    added ConvertFileHelper

---
+++ Infrastructure/Helpers/ConvertFileHelper.cs
@@ -0,0 +1,26 @@
+﻿using Domain.Common;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+
+namespace Infrastructure.Helpers;
+
+public static class ConvertFileHelper
+{
+    public static async Task<FileData> ConvertFromFileToFileDataAsync(IFormFile file)
+    {
+        FileData fileData = new FileData()
+        {
+            ContentType = file.ContentType,
+            FileName = file.ContentType,
+        };
+
+        var fileStream = file.OpenReadStream();
+        var readerFileStream = new StreamReader(fileStream);
+        fileData.Data = Encoding.Unicode.GetBytes(await readerFileStream.ReadToEndAsync());
+
+        fileStream.Dispose();
+        readerFileStream.Dispose();
+
+        return fileData;
+    }
+}
--- WebApi/Controllers/FileConvertController.cs
+++ WebApi/Controllers/FileConvertController.cs
@@ -1,5 +1,6 @@
 using Application.Interfaces.Services;
 using Domain.Common;
+using Infrastructure.Helpers;
 using Microsoft.AspNetCore.Mvc;
 using System.Text;
 
@@ -28,7 +29,7 @@ namespace ConvertHtmlToPdf.Controllers
                 if (!file.ContentType.EndsWith("html"))
                     return BadRequest("Для конвертирования в pdf, отправьте html документ");
 
-                var fileData = await _convertToPdfFileService.ConvertAsync(await ConvertFromFileToFileDataAsync(file));
+                var fileData = await _convertToPdfFileService.ConvertAsync(await ConvertFileHelper.ConvertFromFileToFileDataAsync(file));
                 return File(fileData.Data, fileData.ContentType, fileData.FileName);
             }
             catch (Exception ex)
@@ -36,24 +37,6 @@ namespace ConvertHtmlToPdf.Controllers
                 _logger.Log(LogLevel.Error, ex.Message);
                 return BadRequest("Не удалось конвертировать html в pdf");
             }
-        }
-
-        private async Task<FileData> ConvertFromFileToFileDataAsync(IFormFile file)
-        {
-            FileData fileData = new FileData()
-            {
-                ContentType = file.ContentType,
-                FileName = file.ContentType,
-            };
-
-            var fileStream = file.OpenReadStream();
-            var readerFileStream = new StreamReader(fileStream);
-            fileData.Data = Encoding.Unicode.GetBytes(await readerFileStream.ReadToEndAsync());
-
-            fileStream.Dispose();
-            readerFileStream.Dispose();
-
-            return fileData;
         }
     }
 }
